3dde0d89d81013715c94531ccf06a5e1d9d801aa
mgechev <mgechev@gmail.com>
2015-06-06 18:30:48 +0300

    Update website

---
+++ js/page.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function() {
+	root.mgechev.com.preloader.init();
+	root.mgechev.com.form.init();		
+	$('#homeButton').click(function() {
+		$('#gallery').fadeOut(300, function() {
+			$('.block').fadeIn(300);
+		});
+		return false;
+	});
+	$('#galleryButton').click(function() {
+		$('.block').fadeOut(300, function() {
+			$('#gallery').fadeIn(300);
+		});
+		return false;
+	});
+	$('.sideGallery').click(function() {
+		$('.block').fadeOut(300, function() {
+			$('#gallery').fadeIn(300);
+		});
+		return false;
+	});
+	
+	$('#gallery a').lightBox({ overlayOpacity: 0 });		
+});
---
+++ mail.php
@@ -0,0 +1,21 @@
+﻿<?php
+if (!empty($_POST['email']) && !empty($_POST['content']) &&
+	filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	$to[0] = 'mariela_gencheva@abv.bg';
+	$to[1] = 'mgechev@gmail.com';
+	$content = $_POST['content'];
+	$email = $_POST['email'];
+	$headers = 'From: ' . $email . "\r\n" .
+				'Replay-To: ' . $email . "\r\n";
+	if (empty($_POST['subject'])) {
+		$subject = 'Балабановата къща';
+	} else {
+		$subject = $_POST['subject'];
+	}
+	mail($to[0], $subject, $content, $headers, '-f' . $email);
+	mail($to[1], $subject, $content, $headers, '-f' . $email);
+	echo('{ "success": "Вашето съобщение е изпратено!" }');
+} else {
+	echo('{ "error": "Некоректни данни!" }');
+}
+?>
